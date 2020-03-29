c7f0382b609b6acda5dc1ce8d40daed4a087910b
Yudhistiro Agung <>
2020-03-29 11:16:36 +0700

    install maps on ios

--- app/Screens/Dashboard/Dashboard.component.tsx
+++ app/Screens/Dashboard/Dashboard.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   View, Text,
 } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 import { CovidOverview, CovidCaseList } from '../../Components';
 import { DashboardComponentProps } from './Dashboard.props';
@@ -9,7 +10,22 @@ import style from './Dashboard.style';
 
 const DashboardComponent = ({ cases, ...props }: DashboardComponentProps) => (
   <View style={style.container} >
-    <View style={style.mapContainer} />
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      style={style.mapContainer}
+      initialCamera={{
+        zoom: 8,
+        center: {
+          latitude: -6.9032739,
+          longitude: 107.5729448,
+        },
+        altitude: 10,
+        pitch: 6,
+        heading: 2,
+      }}
+      scrollEnabled={false}
+      zoomEnabled={false}
+    />
     <CovidOverview style={style.overview} {...props} />
     <CovidCaseList data={cases} style={style.detail} />
   </View>
